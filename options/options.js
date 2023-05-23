f829e7187e8ca29a69e653661d7498dcc164eb3c
Crowdbotics <team@crowdbotics.com>
2023-05-23 22:36:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kodi-connect-test-40937",
+  url: "https://kodi-connect-test-40937.botics.co",
+  api: "https://kodi-connect-test-40937.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
